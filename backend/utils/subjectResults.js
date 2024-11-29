72a67502872e1f5cc09ae479582db4147db007f6
obaikoko <jesseobinna7@gmail.com>
2024-11-29 11:35:29 +0100

    updated subjects

--- backend/utils/subjectResults.js
+++ backend/utils/subjectResults.js
@@ -49,7 +49,7 @@ const subjectResults = ({ level }) => {
     return (subjectResults = [
       { subject: 'English' },
       { subject: 'Mathematics' },
-      { subject: 'Social Studies & Citizenship' },
+      { subject: 'Social Studies' },
       { subject: 'Basic Science' },
       { subject: 'Physical And Health Education' },
       { subject: 'Christian Religious Knowledge' },
